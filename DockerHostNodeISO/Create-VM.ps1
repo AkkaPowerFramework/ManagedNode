fa4f1c9147eea6aefcce26f52d43d8591f6bb73b
Wolfgang Stelzhammer <969932+Stelzi79@users.noreply.github.com>
2019-11-29 01:28:14 +0100

    completed the Create-VM.ps1

---
+++ DockerHostNodeISO/Create-VM.ps1
@@ -0,0 +1,67 @@
+# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+    $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments + " -NoProfile -NoLogo"
+    Start-Process -FilePath pwsh.exe -Verb Runas -ArgumentList $CommandLine
+    Exit
+  }
+}
+$ofs = "`n"
+
+$VmName = "AkkaPowerNode"
+# $VmName = "srv"
+
+$ISO = (Get-ChildItem -File -Filter *.iso).FullName
+
+Write-Output "Found following ISO: `n`t$ISO`n"
+
+Write-Output "Checking for existing VMs..."
+
+$VMs = (Get-VM -Name "*$VmName*")
+$VMsNames = ($VMs).Name
+$VMsCount = $VMsNames.Length
+
+$ofs = "`n`t"
+
+Write-Output "VmName: $VmName (found $VMsCount times)`n"
+if ($VMsCount -gt 0) {
+  Write-Output "`t$VMsNames`n"
+}
+
+$ofs = "`n"
+
+$VMsCount += 1
+$NewVMName = "$VmName-" + ([String]$VMsCount).PadLeft(3, "0")
+$NewVHDName = $NewVMName + "-System.vhdx"
+$NewVHDSize = "1GB"
+$NewVHDData = (Get-VMHost).VirtualHardDiskPath + $NewVMName + "-Data.vhdx"
+$NewVHDDataSize = "50GB"
+$NewRamSize = "2GB"
+$NewVMSwitch = (Get-VMHost).ExternalNetworkAdapters[0].SwitchName
+
+Write-Output "Next VM `n`tNode-Name: `t$NewVMName`n`tVHD:`t`t$NewVHDName ($NewVHDSize)`n`tRam-Size: `t$NewRamSize`n`tSwitch:`t`t$NewVMSwitch`n`tData-VHD:`t$NewVHDData ($NewVHDDataSize)`n`n"
+
+Write-Output "Creating new Node ...`n"
+New-VM -Name $NewVMName -NewVHDPath $NewVHDName -NewVHDSizeBytes $NewVHDSize -Generation 2 -SwitchName $NewVMSwitch -MemoryStartupBytes $NewRamSize -BootDevice VHD
+Write-Output "Creating new Data VHDX ..."
+New-VHD $NewVHDData -SizeBytes $NewVHDDataSize
+Write-Output "Attatching Data VHDX ..."
+Add-VMHardDiskDrive -VMName $NewVMName -ControllerType SCSI -ControllerLocation 1 -Path $NewVHDData
+Write-Output "Attatching ISO ..."
+Add-VMDvdDrive -VMName $NewVMName  -ControllerLocation 2 -Path $ISO
+Write-Output "Setting up new NodeVM"
+Set-VMProcessor -VMName $NewVMName -Count ((Get-VMHost).LogicalProcessorCount)
+
+$VMDrives = Get-VMHardDiskDrive -VMName $NewVMName
+$VMDVDs = (Get-VMDvdDrive -VMName $NewVMName)[0]
+
+Set-VMFirmware -VMName $NewVMName -EnableSecureBoot Off -BootOrder $VMDrives[0], $VMDrives[1], $VMDVDs
+
+Start-VM -Name $NewVMName
+
+Write-Output "The Node has now started. Please procede to Configure-Alpine.md"
+Pause
+
+
+
+
